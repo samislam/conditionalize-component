3c3834c13f4f68ed6f2e37ec732d3726e8f4b344
islam yamor <islamyamor@pm.me>
2023-03-28 02:07:41 +0300

    renamed the directory name from dir to dist which was incorrect

--- dir/withConditionals.js
+++ dir/withConditionals.js
@@ -16,24 +16,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 const react_1 = __importDefault(require("react"));
+const flexifyparams_1 = require("flexifyparams");
 function withConditionals(OriginalComponent) {
     return (props) => {
         const f = (0, jsx_runtime_1.jsx)(react_1.default.Fragment, {});
-        const { renderIf, children = f, fallback: fallback_ = f, override: override_ = f, execludeChildren: execludeChildren_ } = props, originalProps = __rest(props, ["renderIf", "children", "fallback", "override", "execludeChildren"]);
+        const _a = (0, flexifyparams_1.flexifyParams)(props), { children = f, execludeChildren = false, fallback = f, override = f, renderIf = true } = _a, originalProps = __rest(_a, ["children", "execludeChildren", "fallback", "override", "renderIf"]);
         switch (true) {
-            case execludeChildren_ === true:
+            case execludeChildren === true:
                 return children;
             case renderIf === true:
                 return (0, jsx_runtime_1.jsx)(OriginalComponent, Object.assign({}, originalProps));
-            case !!override_:
-                return override_;
+            case !!override:
+                return override;
             default:
-                return fallback_;
+                return fallback;
         }
     };
 }
 exports.default = withConditionals;
-function main() {
-    const ComponentC = withConditionals((p) => ((0, jsx_runtime_1.jsx)(react_1.default.Fragment, { children: (0, jsx_runtime_1.jsx)("div", { children: p.name }) })));
-    return (0, jsx_runtime_1.jsx)(ComponentC, { name: "ahmad" });
-}
