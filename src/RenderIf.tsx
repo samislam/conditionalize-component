8359b3a0df6d8ac89048b2f23013f9236386c155
Islam Yamor <islamyamor@gmail.com>
2024-09-28 22:11:39 +0300

    fixed a bug

--- src/RenderIf.tsx
+++ src/RenderIf.tsx
@@ -1,5 +1,5 @@
 import React, { PropsWithChildren } from 'react'
-import withConditionals from 'withConditionals'
+import withConditionals from './withConditionals'
 
 export const RenderIf = withConditionals(({ children, ...props }: PropsWithChildren) => (
   <React.Fragment {...props}>{children}</React.Fragment>
