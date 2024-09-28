3f8e5cfafee559269f7418f3d3bb91e7eed3bc0c
Islam Yamor <islamyamor@gmail.com>
2024-09-28 22:07:32 +0300

    built the project

---
+++ @types/RenderIf.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+export declare const RenderIf: React.ForwardRefExoticComponent<React.RefAttributes<unknown>>;
+export default RenderIf;
+//# sourceMappingURL=RenderIf.d.ts.map
--- @types/withConditionals.d.ts
+++ @types/withConditionals.d.ts
@@ -1,6 +1,5 @@
 import type { FC } from 'react';
 import React from 'react';
-import type { ConditionalProps } from './types';
-declare function withConditionals<PROPS>(OriginalComponent: FC<PROPS>): React.ForwardRefExoticComponent<React.PropsWithoutRef<PROPS & ConditionalProps> & React.RefAttributes<unknown>>;
+declare function withConditionals<PROPS>(OriginalComponent: FC<PROPS>): React.ForwardRefExoticComponent<React.RefAttributes<unknown>>;
 export default withConditionals;
 //# sourceMappingURL=withConditionals.d.ts.map
